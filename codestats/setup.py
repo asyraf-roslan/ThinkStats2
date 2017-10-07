f75d2a695a2cf16bad038d39e43928fdfdafd0ec
Asyraf Roslan <32580801+asyraf-roslan@users.noreply.github.com>
2017-10-07 06:55:59 +0100

    Finished Chapter 1 of ThinkStats2!

--- codestats/setup.py
+++ codestats/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='thinkstats2',
       version='1.0',
-      description="Library code for Think Stats, 2nd Edition packaged",
+      description="Library codestats for Think Stats, 2nd Edition packaged",
       author='Allen Downey',
       license="GPLv3",
       py_modules=['thinkstats2', 'thinkplot'],
