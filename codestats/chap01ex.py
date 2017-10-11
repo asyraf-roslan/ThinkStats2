85a3e8d5d7e915c58b80f5b64b4d5d254e728fdf
Asyraf Roslan <32580801+asyraf-roslan@users.noreply.github.com>
2017-10-11 02:35:59 +0100

    Done exercise 1.2

--- codestats/chap01ex.py
+++ codestats/chap01ex.py
@@ -13,12 +13,59 @@ import sys
 import nsfg
 import thinkstats2
 
+def Read2002FemResp(dct_file="2002FemResp.dct",
+                    dat_file="2002FemResp.dat.gz",
+                    nrows=None):
+    """Reads the 2002 female respondent data
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    return df
+
+def Validate2002Pregnum(resp):
+    """Validate pregnum in the respondent file
+
+    resp: respondent DataFrame
+    """
+    # read the pregnancy frame
+    preg = nsfg.ReadFemPreg()
+
+    # make the map from caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(preg)
+
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.items():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+
+        # check that pregnum from respondent file equals
+        # the number of records in the pregnancy file
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+
+    return True
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    # read and validate respondent file
+    resp = Read2002FemResp()
+
+    assert(len(resp) == 7643)
+    assert(resp.pregnum.value_counts()[1] == 1267)
+
+    # validate that the pregnum column in respondent matches the number
+    # of entries in pregnancy
+    assert(Validate2002Pregnum(resp))
+
     print('%s: All tests passed.' % script)
 
 
